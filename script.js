68d1694dd63d8896206d9752099003798b2043e4
Mamiyamatsu <szalewskifilip@gmail.com>
2025-03-21 23:57:05 +0100

    Slider article menu

--- script.js
+++ script.js
@@ -32,3 +32,25 @@ document.addEventListener("DOMContentLoaded", function () {
     xhrContact.send();
   });
 });
+
+let currentSlide = 0;
+const slides = document.querySelector(".slides");
+const totalSlides = slides.children.length;
+
+function showSlide(index) {
+  if (index >= totalSlides) {
+    currentSlide = 0;
+  } else if (index < 0) {
+    currentSlide = totalSlides - 1;
+  } else {
+    currentSlide = index;
+  }
+  const offset = -currentSlide * 100;
+  slides.style.transform = `translateX(${offset}%)`;
+}
+
+function autoSlide() {
+  showSlide(currentSlide + 1);
+}
+
+let slideInterval = setInterval(autoSlide, 5000);
